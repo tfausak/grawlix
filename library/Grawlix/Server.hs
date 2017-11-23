74eaf1be5c58660e17e2d3ef6012268b2c1a484d
Taylor Fausak <taylor@fausak.me>
2017-11-23 14:08:01 -0600

    Fix some formatting

--- library/Grawlix/Server.hs
+++ library/Grawlix/Server.hs
@@ -1,9 +1,10 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeOperators #-}
 
-module Grawlix.Server (main) where
+module Grawlix.Server
+  ( main
+  ) where
 
-import Flow ((|>))
 import Grawlix.Database
 import Grawlix.Handler.GetHealthCheck
 import Grawlix.Handler.GetLibraries
@@ -11,6 +12,7 @@ import Grawlix.Handler.GetModules
 import Grawlix.Handler.GetPackages
 import Grawlix.Handler.GetRevisions
 import Grawlix.Handler.GetVersions
+import Servant ((:<|>)((:<|>)))
 
 import qualified Hasql.Connection as Sql
 import qualified Network.Wai as Wai
@@ -19,47 +21,38 @@ import qualified Network.Wai.Middleware.Gzip as Middleware
 import qualified Network.Wai.Middleware.RequestLogger as Middleware
 import qualified Servant
 
-
 main :: IO ()
 main = do
   connection <- getConnection
-  connection |> applicationWith |> middleware |> Warp.runSettings settings
-
+  Warp.runSettings settings . applyMiddleware $ makeApplication connection
 
 settings :: Warp.Settings
-settings = Warp.defaultSettings
-  |> Warp.setBeforeMainLoop (putStrLn "Starting server ...")
-  |> Warp.setPort 8080
-  |> Warp.setServerName mempty
+settings =
+  Warp.setBeforeMainLoop (putStrLn "Starting server ...") . Warp.setPort 8080 $
+  Warp.setServerName mempty Warp.defaultSettings
 
+applyMiddleware :: Wai.Middleware
+applyMiddleware = Middleware.gzip Middleware.def . Middleware.logStdout
 
-middleware :: Wai.Middleware
-middleware application = application
-  |> Middleware.gzip Middleware.def
-  |> Middleware.logStdout
-
-
-applicationWith :: Sql.Connection -> Wai.Application
-applicationWith connection = connection |> serverWith |> Servant.serve api
-
+makeApplication :: Sql.Connection -> Wai.Application
+makeApplication = Servant.serve api . makeServer
 
 api :: Servant.Proxy Api
 api = Servant.Proxy
 
-
 type Api
-  = GetHealthCheck
-  Servant.:<|> GetPackages
-  Servant.:<|> GetVersions
-  Servant.:<|> GetRevisions
-  Servant.:<|> GetLibraries
-  Servant.:<|> GetModules
+   = GetHealthCheck
+     :<|> GetPackages
+     :<|> GetVersions
+     :<|> GetRevisions
+     :<|> GetLibraries
+     :<|> GetModules
 
-serverWith :: Sql.Connection -> Servant.Server Api
-serverWith connection
-  = getHealthCheckHandler connection
-  Servant.:<|> getPackagesHandler connection
-  Servant.:<|> getVersionsHandler connection
-  Servant.:<|> getRevisionsHandler connection
-  Servant.:<|> getLibrariesHandler connection
-  Servant.:<|> getModulesHandler connection
+makeServer :: Sql.Connection -> Servant.Server Api
+makeServer connection =
+  getHealthCheckHandler connection :<|> -- Force hindent newline.
+  getPackagesHandler connection :<|>
+  getVersionsHandler connection :<|>
+  getRevisionsHandler connection :<|>
+  getLibrariesHandler connection :<|>
+  getModulesHandler connection
