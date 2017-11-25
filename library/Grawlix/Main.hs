88a124c4986c582556d46700806e28ca1a3d960e
Taylor Fausak <taylor@fausak.me>
2017-11-25 09:38:57 -0600

    Continuously run sync

--- library/Grawlix/Main.hs
+++ library/Grawlix/Main.hs
@@ -9,7 +9,10 @@ import Grawlix.Server
 import Grawlix.Sync
 import Grawlix.Type.Config
 
+import qualified Control.Concurrent as Concurrent
+import qualified Control.Concurrent.Async as Async
 import qualified Control.Monad as Monad
+import qualified Hasql.Connection as Sql
 
 main :: IO ()
 main = do
@@ -17,5 +20,23 @@ main = do
   config <- getConfig options
   connection <- getConnection config
   runMigrations config connection
-  Monad.when (configSyncEnabled config) (runSync config connection)
-  Monad.when (configServerEnabled config) (runServer connection)
+  Async.race_
+    (runSyncForever config connection)
+    (runServerForever config connection)
+
+runSyncForever :: Config -> Sql.Connection -> IO ()
+runSyncForever config connection =
+  if configSyncEnabled config
+    then Monad.forever $ do
+           runSync config connection
+           Concurrent.threadDelay 60000000 -- 1 minute
+    else sleepForever
+
+runServerForever :: Config -> Sql.Connection -> IO ()
+runServerForever config connection =
+  if configServerEnabled config
+    then runServer connection
+    else sleepForever
+
+sleepForever :: IO ()
+sleepForever = Monad.forever $ Concurrent.threadDelay 1000000 -- 1 second
